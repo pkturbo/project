119af7b8a9598ed556265159d0380f92d5c9ac7b
pkturbo <pkturbo@gmail.com>
2015-07-20 23:22:29 -0400

    mostly working, memory issues on laptop

--- project.R
+++ project.R
@@ -5,21 +5,44 @@
 # load libraries
 library(caret)
 
-# train a random forest model
+# this file is for training and testing my model
+train.Data <- read.csv(file = 'pml-training.csv',stringsAsFactors = FALSE,
+                       na.strings = c("NA","#DIV/0!",""))
+train.Data$classe <- as.factor(train.Data$classe)
 
-# this file is for training and testing your model
-train.Data <- read.csv(file='pml-training.csv')
+# filter out NAs, DIV/0, blank columns
+temp.Data <- train.Data[train.Data$new_window == "no",seq(7,160)]
+bad <- logical(154)
+cs <- colSums(temp.Data[,-154])
+bad <- complete.cases(cs)
+bad[154] <- FALSE
+train.Data2 <- temp.Data[,c(1:5,31:43,54:62,96,107:118,134,145:154)]
+
 
 # create training and testing data sets
-inTraining <- createDataPartition(y=train.Data$classe,p=0.7,list=FALSE)
-training <- train.Data[inTraining,]
-testing <- train.Data[-inTraining,]
+inTraining <- createDataPartition(y = train.Data2$classe,p = 0.7,list = FALSE)
+training <- train.Data2[inTraining,]
+testing <- train.Data2[-inTraining,]
 
-# explore the data set
-featurePlot(training[,-160],training$classe)
-
-# preprocess the training data
-preObj <- preProcess(training[,-classe],method="knnImpute")
-
-modFit <- train(classe ~ .,data=training,)
+# preprocess and train a random forest model
+# using smaller test set due to memory constraints
+modFit <- train(classe ~ .,data=testing,method="rf",
+                preProcess=c("center","scale"),prox=TRUE)
+save(modFit,file='modFitRf.Rdata')
 modFit
+
+pred <- predict(modFit,data=training)
+table(pred,testing$classe)
+
+
+
+
+pml_write_files = function(x) {
+        n = length(x)
+        for (i in 1:n) {
+                filename = paste0("problem_id_",i,".txt")
+                write.table(
+                        x[i],file = filename,quote = FALSE,row.names = FALSE,col.names = FALSE
+                )
+        }
+}
